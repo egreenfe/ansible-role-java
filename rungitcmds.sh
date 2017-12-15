7b0f231c4bcd02801f48a10547e913bb94d5bb6b
egreenfe <egreenfe@gmail.com>
2017-12-15 13:03:46 -0500

    First Commit

---
+++ rungitcmds.sh
@@ -0,0 +1,6 @@
+echo "# ansible-role-java" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/egreenfe/ansible-role-java.git
+git push -u origin master
